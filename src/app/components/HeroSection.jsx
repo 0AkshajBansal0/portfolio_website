59a80b023c77ca7e5382fa46d60b3957fab4c511
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-02-18 00:15:35 +0530

    minor changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,12 +35,12 @@ const HeroSection = () => {
                     <div>
                         <Link
                             href="/#contact"
-                            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white transform transition-all duration-300 ease-in-out hover:scale-105">
                             Hire Me
                         </Link>
                         <Link
                             href="/"
-                            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+                            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3 transform transition-all duration-300 ease-in-out hover:scale-105">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                                 Download CV
                             </span>
@@ -48,11 +48,11 @@ const HeroSection = () => {
                     </div>
                 </div>
                 <div className="col-span-4 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden shadow-lg hover:scale-110 transform transition-all duration-300 ease-in-out">
                         <Image
                             src="/images/hero-image.png"
                             alt="hero image"
-                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-auto h-[250px] lg:h-[400px]"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-auto h-[250px] lg:h-[400px] object-cover"
                             width={300}
                             height={300}
                         />
