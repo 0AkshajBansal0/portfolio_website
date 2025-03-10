5d69f7bb88376101d5b9146091304aef01339107
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-03-10 22:20:08 +0530

    education data

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+    export const EDUCATION = [
+        {
+            year: "2023 - 2027",
+            role: "B.Tech in Computer Science Engineering",
+            company: "SRM Institute of Science and Technology, Kattankulathur",
+            description: `I am currently pursuing a B.Tech degree at SRMIST. I achieved a 10.00 GPA in my first semester, followed by a 9.90 GPA in the second semester, resulting in an impressive cumulative CGPA of 9.95.`,
+            technologies: ["Python", "C++", "Java", "HTML5", "CSS3", "JS", "React.js", "FireBase", "mongoDB", "MySQL"],
+        },
+        {
+            year: "2022 - 2023",
+            role: "12th Class",
+            company: "V.V. D.A.V. Public School, Delhi",
+            description: `Passed with 85.6% in 12th Board Exams`,
+            technologies: ["Python", "MySQL"],
+        },
+        {
+            year: "2020 - 2021",
+            role: "10th Class",
+            company: "V.V. D.A.V. Public School, Delhi",
+            description: `Passed with 96.2% in 10th CBSE Exams`,
+            technologies: [],
+        },
+    ];
