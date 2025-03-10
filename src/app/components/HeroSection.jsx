7c2e152ba229b3d4f588f567117f309c72042cb8
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-03-10 22:07:21 +0530

    hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 const HeroSection = () => {
     return (
@@ -13,7 +15,7 @@ const HeroSection = () => {
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                             Hello, I'm{" "}
                         </span>
-                        <br></br>
+                        <br />
                         <TypeAnimation
                             sequence={[
                                 "Akshaj Bansal",
@@ -29,17 +31,42 @@ const HeroSection = () => {
                         />
                     </h1>
                     <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                        voluptuous.
+                        Crafting innovative solutions with technology,
+                        <br className="hidden sm:block" />
+                        blending creativity and functionality to make an impact.
                     </p>
-                    <div>
+
+
+                    {/* Social Media Icons */}
+                    <div className="flex justify-center sm:justify-start space-x-6">
                         <Link
-                            href="/#contact"
-                            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white transform transition-all duration-300 ease-in-out hover:scale-105">
-                            Hire Me
+                            href="https://github.com/0AkshajBansal0"
+                            target="_blank"
+                            className="text-white text-3xl hover:text-gray-400 transition-all duration-200"
+                        >
+                            <FaGithub />
                         </Link>
                         <Link
-                            href="/"
+                            href="https://www.linkedin.com/in/akshaj-bansal-44a6211b8"
+                            target="_blank"
+                            className="text-white text-3xl hover:text-blue-400 transition-all duration-200"
+                        >
+                            <FaLinkedin />
+                        </Link>
+                        <Link
+                            href="https://leetcode.com/u/akshajbansal"
+                            target="_blank"
+                            className="group text-3xl transition-all duration-200"
+                        >
+                            <SiLeetcode className="text-white group-hover:text-yellow-400 transition-all duration-200" />
+                        </Link>
+
+                    </div>
+
+                    {/* Download CV Button */}
+                    <div className="mt-6">
+                        <Link
+                            href="https://drive.google.com/file/d/1UXMKwI93KZzFFaj7dfiC8VQTiToJD6O3/view?usp=sharing"
                             className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3 transform transition-all duration-300 ease-in-out hover:scale-105">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                                 Download CV
@@ -47,8 +74,10 @@ const HeroSection = () => {
                         </Link>
                     </div>
                 </div>
+
+                {/* Hero Image */}
                 <div className="col-span-4 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden shadow-lg hover:scale-110 transform transition-all duration-300 ease-in-out">
+                    <div className="rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden shadow-lg hover:scale-110 transform transition-all duration-300 ease-in-out">
                         <Image
                             src="/images/hero-image.png"
                             alt="hero image"
@@ -57,6 +86,7 @@ const HeroSection = () => {
                             height={300}
                         />
                     </div>
+
                 </div>
             </div>
         </section>
