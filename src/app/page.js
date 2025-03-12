981b6e80c483400991fe84d5184f4c9c6e5ce090
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-03-12 10:20:50 +0530

    page & index

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,40 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Education from "./components/Education";
+import AboutSection from "./components/AboutSection";
+import Experience from "./components/Experience";
+import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import AchievementsSection from "./components/AchievementsSection";
+import Footer from "./components/Footer";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
+      <section id="hero">
+          <HeroSection />
+        </section>
+        <AchievementsSection />
+        <section id="about">
+          <AboutSection />
+        </section>
+        <section id="experience">
+          <Experience />
+        </section>
+        <section id="education">
+          <Education />
+        </section>
+        <section id="projects">
+          <ProjectsSection />
+        </section>
+        <section id="contact">
+          <EmailSection />
+        </section>
+        <Footer />
       </div>
     </main>
   );
